b34b82a298fbaa02b3c573f90491ffe0582e3deb
Aaron Martinez <aaronmarti17@gmail.com>
2019-08-22 07:17:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,7 +7,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = true)
   total = starting_point
   for it in source_array do
     total = yield(total, it)
